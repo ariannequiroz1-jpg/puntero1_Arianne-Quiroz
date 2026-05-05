93199fc81b695c0adc4621fa86313ee04fcdc095
Laboratorio <laboratorio@udla-ec.int>
2026-05-05 12:01:21 -0500

    Fase final: Se mejora y completa la estructura del codigo

--- punteros1.c
+++ punteros1.c
@@ -2,18 +2,19 @@
 int main(){
     int num=15, a;
     int *x;
-    a = *x;
+    a = *x; // a se le asigna el valor de x
     x = &num; //x se le asigna a la direccion de num
-    printf("%p %d\n",x);
+    printf("%p %d\n",x); // traabajatr con caracteres
     char letra = 'a' , c;
     char *ptr;
     ptr = &letra;
     c= *ptr;
-    printf("%p %c\n",ptr , c);
+    printf("%p %c\n",ptr , c); // trabajo con decimales 
     float decimal= 3.14, dec;
     float *ptrF;
     ptrF = &decimal;
     dec = *ptrF;
     printf("%p %c\n",ptrF , dec);
+    return 0;
     
 }
